00d2ae2e1547d48cd5723364d1d0ecca0056e18c
ani-sha22 <anisha.chakraborty22@gmail.com>
2023-02-28 20:08:07 +0530

    a patch

--- input-output.py
+++ input-output.py
@@ -3,6 +3,7 @@ from division import div
 from multiply import mult
 from subtraction import subtract
 from mod import mod
+from percent import percent
 
 a = int(input("Enter num 1 = "))
 b = int(input("Enter num 2 = "))
@@ -13,3 +14,4 @@ elif(op == '-'): print("\nOutput = ",subtract(a,b))
 elif(op == '*'): print("\nOutput = ",mult(a,b))
 elif(op == '/'): print("\nOutput = ",div(a,b))
 elif(op == "%"): print("\nOutput = ",mod(a,b))
+elif(op == "p"): print("\nOutput = ",percent(a,b))
