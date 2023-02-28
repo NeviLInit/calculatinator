c75469591d7a522c31478ecbe67aa605dce84cbf
NeviLInit <antarikshkalbande27@gmail.com>
2023-02-28 22:38:01 +0530

    antarikshpatch

--- input-output.py
+++ input-output.py
@@ -4,6 +4,7 @@ from multiply import mult
 from subtraction import subtract
 from mod import mod
 from percent import percent
+from log import lo
 
 a = int(input("Enter num 1 = "))
 b = int(input("Enter num 2 = "))
@@ -15,3 +16,4 @@ elif(op == '*'): print("\nOutput = ",mult(a,b))
 elif(op == '/'): print("\nOutput = ",div(a,b))
 elif(op == "%"): print("\nOutput = ",mod(a,b))
 elif(op == "p"): print("\nOutput = ",percent(a,b))
+elif(op == "l"): print("\nOutput = ",lo(a))
